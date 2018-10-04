8a58a67aa7ecd1e069407495231e22c22338c59f
Källström, Maria <maria.kallstrom@sogeti.com>
2018-10-04 11:25:53 +0200

    commit

--- NalaApplication/Controllers/SampleDataController.cs
+++ NalaApplication/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace NalaApplication.Controllers
 	{
 		private static string[] Summaries = new[]
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltring", "Scorching"
 		};
 
 		[HttpGet("[action]")]
