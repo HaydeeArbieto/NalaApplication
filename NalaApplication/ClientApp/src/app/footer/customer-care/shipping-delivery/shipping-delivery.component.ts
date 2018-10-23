71526b585674ffee6e2dba006b0bc62b7b27e415
Arbieto, Haydee <haydee.arbieto@sogeti.com>
2018-10-23 13:48:26 +0200

    Fixed design in some pages

---
+++ NalaApplication/ClientApp/src/app/footer/customer-care/shipping-delivery/shipping-delivery.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //selector: 'xxx',
+  templateUrl: './shipping-delivery.component.html',
+  styleUrls: ['./shipping-delivery.component.css'],
+})
+export class ShippingComponent {
+}
