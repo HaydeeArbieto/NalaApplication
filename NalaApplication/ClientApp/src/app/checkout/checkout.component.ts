33c39e511ad415bf301bc1fb81c30a0e121892ea
Arbieto, Haydee <haydee.arbieto@sogeti.com>
2018-10-30 14:38:26 +0100

    Continue to fixed checkout files

--- NalaApplication/ClientApp/src/app/checkout/checkout.component.ts
+++ NalaApplication/ClientApp/src/app/checkout/checkout.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'XX',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent {
+
+  public show: boolean = false;
+  public buttonName: any = 'Yes';
+
+  ngOnInit() { }
+
+  toggle() {
+    this.show = !this.show;
+
+    // Change the name of buttom
+    if (this.show)
+      this.buttonName = "No";
+    else
+      this.buttonName = "Yes";
+  }
 }
